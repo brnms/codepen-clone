9d2c392f6579f2fed7ee0b4d7d91d7d30a9fabec
Bruno Santos <bruno.matasantos@gmail.com>
2020-11-06 10:22:43 -0300

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Editor from './Editor'
 
 function App() {
   const [html, setHtml] = useState('')
   const [css, setCss] = useState('')
   const [js, setJs] = useState('')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc (
+        `
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `
+      )
+    }, 1000)
+
+    return () =>  clearTimeout(timeout)
+  },[html, css, js])
   return (
     <>
     <div className="pane top-pane">
@@ -19,15 +36,16 @@ function App() {
        value={css}
        onChange={setCss} />
       <Editor
-        language="js"
+        language="javascript"
         displayName="JS"
         value={js}
         onChange={setJs} />
     </div>
     <div className="pane">
         <iframe 
+        srcDoc={srcDoc}
           title="output"
-          sandbox="allow-script"
+          sandbox="allow-scripts"
           frameBorder="0"
           width="100%"
           height="100%"
